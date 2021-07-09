8a4ca984ce27e97aadb67cc6acfdf06ade1b5c54
saad <saadrehman2000@gmail.com>
2021-07-09 21:34:17 +0400

    firebase issue with going over daily read quota, added 'like' button on posts

--- src/components/post/actions.js
+++ src/components/post/actions.js
@@ -3,27 +3,53 @@ import PropTypes from 'prop-types';
 import FirebaseContext from "../../context/firebase";
 import UserContext from "../../context/user";
 
-export default function Actions({docId, totalLikes, likedPhoto, handleFocus}) {
+export default function Actions({ docId, totalLikes, likedPhoto, handleFocus }) {
     const {
         user: {
             uid: userId = ''
         }
     } = useContext(UserContext);
-    
+
     const [likes, setLikes] = useState(totalLikes);
     const [toggleLiked, setToggleLiked] = useState(likedPhoto);
     const { firebase, FieldValue } = useContext(FirebaseContext);
 
     const handleToggleLiked = async () => {
         setToggleLiked(!toggleLiked);
-    }
 
-    await firebase.firestore()
-    .collection('photos').doc(docId)
-    .update({
-        liked: toggleLiked ? FieldValue.arrayRemove(userId)
-        : FieldValue.arrayUnion(userId)
-    });
 
-    
+        await firebase.firestore()
+            .collection('photos').doc(docId)
+            .update({
+                liked: toggleLiked ? FieldValue.arrayRemove(userId)
+                    : FieldValue.arrayUnion(userId)
+            });
+        
+        setLikes((likes) => (toggleLiked ? likes + 1 : likes - 1));
+    };
+
+    return (
+        <> 
+        <div className="flex justify-between p-2">
+            <div className="flex">
+            <svg 
+            xmlns="http://www.w3.org/2000/svg" 
+            className={`h-8 w-8 ml-2 ${toggleLiked ? 'fill-red text-red-primary' : 'text-black-light'}`} 
+            onClick={handleToggleLiked}
+            fill="none" 
+            viewBox="0 0 24 24" 
+            stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+            </svg>
+            </div>
+        </div>
+        </>
+    )
+}
+
+Actions.propTypes = {
+    docId: PropTypes.string.isRequired,
+    totalLikes: PropTypes.number.isRequired,
+    likedPhoto: PropTypes.bool.isRequired,
+    handleFocus: PropTypes.func.isRequired
 }
--- src/components/post/index.js
+++ src/components/post/index.js
@@ -2,14 +2,24 @@ import { useRef } from "react"
 import PropTypes from 'prop-types';
 import Header from "./Header";
 import Image from './Image';
+import Actions from "./actions";
 
-export default function Post({content}) {
+export default function Post({ content }) {
+    const commentInput = useRef(null);
+    const handleFocus = () => commentInput.current.focus();
+
     return (
-        <div className="rounded col-span-4 bg-white border border-gray-primary mb-16 mt-4">
-        <Header username={content.username} />
-        <Image src={content.imageSrc} caption={content.caption} />
+        <div className="rounded col-span-4 bg-white border border-gray-primary mb-16 mt-4 w-11/12">
+            <Header username={content.username} />
+            <Image src={content.imageSrc} caption={content.caption} />
+            <Actions 
+                docId={content.docId}
+                totalLikes={content.likes.length}
+                likedPhoto={content.userLikedPhoto}
+                handleFocus={handleFocus}
+            />
         </div>
-    ) 
+    )
 }
 
 Post.propTypes = {
